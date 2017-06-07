11c0ae9581f8e4775a2482a1aa5a5619eaa586d7
clwangchunlei <13426259587@163.com>
2017-06-08 02:18:23 +0800

    1
    V1.0

---
+++ Software/SoftwareFunctions/wangchunlei/Service-Translator-Instrument/Service-Translator-Instrument.py
@@ -0,0 +1,19 @@
+from time import sleep
+import autoit
+import os,sys
+
+baseline = os.path.dirname(os.path.realpath(__file__)) + '\\CSV\\sslf_imp.txt'
+
+print "__file__=%s" % baseline
+autoit.win_activate('CentraLink')
+autoit.send("{ESC 3}")
+autoit.send("!S S D I")
+sleep(0.2)
+autoit.win_activate('CentraLink')
+autoit.send("{space}")
+autoit.send("{TAB}")
+autoit.send(baseline)
+sleep(0.5)
+autoit.control_command( "CentraLink", "Button7", "Check")
+sleep(5)
+autoit.send("{ESC}")
