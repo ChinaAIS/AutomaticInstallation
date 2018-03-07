9d7c06a45b3a4221972a01397d678292c83e73b0
gzmaomao <sccdxin@163.com>
2018-03-07 22:55:56 +0800

    修改日志记录

--- Software/CentralinkFunctions/Xiezhongxin/GenerateTestMethodCode/GenerateTMC.py
+++ Software/CentralinkFunctions/Xiezhongxin/GenerateTestMethodCode/GenerateTMC.py
@@ -3,24 +3,22 @@ import os,sys,time,datetime,shutil
 import configparser,xlrd,autoit
 import win32com.client
 
-now=datetime.datetime.now()
 
 # Start Define Function
 
+#记录并显示日志
 def WriteLogFile(strLogMessage):
-      #now=datetime.datetime.now()
+      now=datetime.datetime.now()
       LogFileName= os.getcwd()+r'\Log\\'+str(now.strftime('%Y%m%d'))+'.log'
       if os.path.exists(LogFileName):
             LogFile=open(LogFileName,'a')
-            LogFile.write('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+strLogMessage+'\n')
-            LogFile.close()
       else:
             LogFile=open(LogFileName,'w')
-            LogFile.write('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+strLogMessage+'\n')
-            LogFile.close()   
-
+      LogFile.write('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+strLogMessage+'\n')
+      LogFile.close()   
+      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+strLogMessage+'\n')
+      
 #根据test.xlsx文件生成文件:test.csv,mtd.csv,tstc.csv,sslf_imp.txt,Analyzer-Test-Codes.ini,Test-Information.ini
-
 def GenerateTestCsvFile():
       #打开指定文件路径的excel文件  
       xlsfile = os.getcwd()+r'\doc\test.xlsx'
@@ -34,31 +32,26 @@ def GenerateTestCsvFile():
       testFile=open(csvFilePath+r'\test.csv','w')
       testFile.write('Name,Description:E,SampleType.Name,OmitOnLASUpdate,PatientDataType,ControlDataType,PatientUnit.Name,ControlUnit.Name,AlternateUnit.Name,UnitCoefficient,UnitTerm,SeqNo,DefaultValue,Calculation,CalcNormSevLimit,CalcDeltaNormSevLimit,CalcQCSevLimit,CalcInstSevLimit,EditWarning,DeltaCheckLimit,AllowAutovalidation,AllowLISDownload,LISUploadMode,ControlReportDecimals,PatientReportDecimals,AllowLASDownload,Trigger.Description,OnNewRequest.Description,~_DeltaNormsForCompActive\n')
       testFile.close();
-      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Create test.csv File')
       WriteLogFile('Create test.csv File')
 
       mtdFile=open(csvFilePath+r'\mtd.csv','w')
       mtdFile.write('Test.Name,Instrument.Name,Westgard12sSeverity,Westgard13sSeverity,Westgard10xSeverity,Westgard22sSeverity,Westgard41sSeverity,WestgardR4sSeverity,ApplyWestgardChecks,QCSeverity,Unit.Name,LowDetectionLimit,HighDetectionLimit,EnableTargeting,AutoTargetSeqNo,EnableWorkOrders,DownloadDilution,CheckMovingAverage,QCSeverityOnTypeError,QCSeverityOnInvalidRef,ValidateOnTypeError,ValidateOnInvalidRange,NormSeverityLimit,DeltaNormSeverityLimit,QCSeverityLimit,InstSeverityLimit,CounterKeyLabel,ReagentInformation,WGCheckIfCompForQC,InstrRerunSelection,PatientResultSelector.Name,ControlResultSelector.Name,OnQualityResultProcess.Description,LASTestAvailAlias.Test.Name\n')
       mtdFile.close();
-      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Create mtd.csv File')
       WriteLogFile('Create mtd.csv File')
       
       tstcFile=open(csvFilePath+r'\tstc.csv','w')
       tstcFile.write('CodingSystem.Name,Test.Name,InboundValue,OutboundValue\n')
       tstcFile.close();
-      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Create tstc.csv File')
       WriteLogFile('Create tstc.csv File')
       
       AnalyzerTestCodesFile=open(csvFilePath+r'\Analyzer-Test-Codes.ini','w')
       AnalyzerTestCodesFile.write('ANALYZER TYPE'+'\t'+'ANALYZER TEST CODE'+'\t'+'ASTM TEST CODE'+'\t'+'TEST NAME'+'\t'+'ORDER DESPITE ZERO INVENTORY'+'\t'+'COMBI TESTS\n')
       AnalyzerTestCodesFile.close();
-      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Create Analyzer-Test-Codes.ini File')
       WriteLogFile('Create Analyzer-Test-Codes.ini File')
       
       TestInformationFile=open(csvFilePath+r'\Test-Information.ini','w')
       TestInformationFile.write('ASTM TEST CODE'+'\t'+'TEST CODE'+'\t'+'ASTM TEST CODE'+'\t'+'TEST DESCRIPTION'+'\t'+'NODE TYPE'+'\t'+'DISPOSING TIME (HRS)\n')
       TestInformationFile.close();
-      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Create Test-Information.ini File')
       WriteLogFile('Create Test-Information.ini File')
       
       testFile=open(csvFilePath+r'\test.csv','a')
@@ -77,7 +70,6 @@ def GenerateTestCsvFile():
                   strDataType= sheet.cell(i,2).value
                   strUnit= sheet.cell(i,3).value
                   testFile.write(strTestName+','+strDescription+','+strSampleType+',no,'+strDataType+','+strDataType+','+strUnit+','+strUnit+','+'?,?,?,?,?,?,?,?,?,?,?,?,yes,yes,Allow always,?,?,yes,?,?,?'+'\n')
-                  print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Write \''+strTestName+','+strDescription+','+strSampleType+',no,'+strDataType+','+strDataType+','+strUnit+','+strUnit+','+'?,?,?,?,?,?,?,?,?,?,?,?,yes,yes,Allow always,?,?,yes,?,?,?'+'\' to test.csv')
                   WriteLogFile('Write \''+strTestName+','+strDescription+','+strSampleType+',no,'+strDataType+','+strDataType+','+strUnit+','+strUnit+','+'?,?,?,?,?,?,?,?,?,?,?,?,yes,yes,Allow always,?,?,yes,?,?,?'+'\' to test.csv')
                   
                   #2)write to mtd.csv 
@@ -95,7 +87,6 @@ def GenerateTestCsvFile():
                                                 if strInstrumentType=='ACH':
                                                       strInstrumentName='ChemistryXPT_'+arrInstrumentList[j]
                                     mtdFile.write(strTestName+','+strInstrumentName+',1,1,1,1,1,1,yes,0,?,?,?,no,?,yes,no,no,5,5,no,no,6,6,6,6,?,?,no,Any,?,?,?,?'+'\n')
-                                    print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Write \''+strTestName+','+strInstrumentName+',1,1,1,1,1,1,yes,0,?,?,?,no,?,yes,no,no,5,5,no,no,6,6,6,6,?,?,no,Any,?,?,?,?'+'\' to mtd.csv')
                                     WriteLogFile('Write \''+strTestName+','+strInstrumentName+',1,1,1,1,1,1,yes,0,?,?,?,no,?,yes,no,no,5,5,no,no,6,6,6,6,?,?,no,Any,?,?,?,?'+'\' to mtd.csv')
                         else:
                               if strInstrumentType=='A24':
@@ -107,7 +98,6 @@ def GenerateTestCsvFile():
                                           if strInstrumentType=='ACH':
                                                 strInstrumentName='ChemistryXPT_'+strInstrumentList
                               mtdFile.write(strTestName+','+strInstrumentName+',1,1,1,1,1,1,yes,0,?,?,?,no,?,yes,no,no,5,5,no,no,6,6,6,6,?,?,no,Any,?,?,?,?'+'\n')
-                              print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Write \''+strTestName+','+strInstrumentName+',1,1,1,1,1,1,yes,0,?,?,?,no,?,yes,no,no,5,5,no,no,6,6,6,6,?,?,no,Any,?,?,?,?'+'\' to mtd.csv')
                               WriteLogFile('Write \''+strTestName+','+strInstrumentName+',1,1,1,1,1,1,yes,0,?,?,?,no,?,yes,no,no,5,5,no,no,6,6,6,6,?,?,no,Any,?,?,?,?'+'\' to mtd.csv')
                               
                   #3)write to tstc.csv
@@ -116,35 +106,27 @@ def GenerateTestCsvFile():
                         if strInstrumentType=='A24':
                               strCodingSystem='Advia 2400'
                               tstcFile.write(strCodingSystem+','+strTestName+','+strBoundValue+','+strBoundValue+'\n')
-                              print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Write \''+strCodingSystem+','+strTestName+','+strBoundValue+','+strBoundValue+'\' to tstc.csv')
                               WriteLogFile('Write \''+strCodingSystem+','+strTestName+','+strBoundValue+','+strBoundValue+'\' to tstc.csv')
                   #4)write to Analyzer-Test-Codes.ini
                   AnalyzerTestCodesFile.write(strInstrumentType+'\t'+strTestName+'\t'+strTestName+'\t'+strTestName+'\t'+'NO'+'\t'+'\n')
-                  print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Write \''+strCodingSystem+','+strInstrumentType+'\t'+strTestName+'\t'+strTestName+'\t'+strTestName+'\t'+'NO'+'\t'+'\' to Analyzer-Test-Codes.ini.csv')
                   WriteLogFile('Write \''+strCodingSystem+','+strInstrumentType+'\t'+strTestName+'\t'+strTestName+'\t'+strTestName+'\t'+'NO'+'\t'+'\' to Analyzer-Test-Codes.ini.csv')
                   #5)write to Test-Information.ini
                   TestInformationFile.write(strTestName+'\t'+strTestName+'\t'+strInstrumentType+'-'+strTestName+'\t'+strInstrumentType+'\t'+'\n')
-                  print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Write \''+strTestName+'\t'+strTestName+'\t'+strInstrumentType+'-'+strTestName+'\t'+strInstrumentType+'\t'+'\' to Test-Information.ini')
                   WriteLogFile('Write \''+strTestName+'\t'+strTestName+'\t'+strInstrumentType+'-'+strTestName+'\t'+strInstrumentType+'\t'+'\' to Test-Information.ini')
                   
       testFile.close();
-      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Generate test.csv successful')
       WriteLogFile('Generate test.csv successful')
       
       mtdFile.close();
-      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Generate mtd.csv successful')
       WriteLogFile('Generate mtd.csv successful')
       
       tstcFile.close();
-      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Generate tstc.csv successful')
       WriteLogFile('Generate tstc.csv successful')
       
       AnalyzerTestCodesFile.close();
-      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Generate Analyzer-Test-Codes.ini successful')
       WriteLogFile('Generate Analyzer-Test-Codes.ini successful')
       
       TestInformationFile.close();
-      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Generate Test-Information.ini successful')
       WriteLogFile('Generate Test-Information.ini successful')
       
       #write sslf_imp.txt
@@ -156,9 +138,9 @@ def GenerateTestCsvFile():
       sslf_impFile.write('cntc.csv ?\n')
       sslf_impFile.write('tstc.csv ?\n')
       sslf_impFile.close();
-      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Generate sslf_imp.txt successful')
       WriteLogFile('Generate sslf_imp.txt successful')
       
+#判断某个程序是否运行(方法一)      
 def isRun(ProcessName):
       isRunFlag=False
       try:
@@ -172,7 +154,8 @@ def isRun(ProcessName):
             isRunFlag=False
       finally:
             return isRunFlag
-      
+
+#判断某个程序是否运行(方法二)     
 def isRunning(ProcessName):
       isRunFlag=False
       try:
@@ -184,10 +167,10 @@ def isRunning(ProcessName):
             isRunFlag=False
       finally:
             return isRunFlag
-      
+
+#登录Centralink      
 def LoginCentralink():
       if isRunning('prowin32.exe'):
-            print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Centralink is already running')
             WriteLogFile('Centralink is already running')
             # at Login Form, But can't Login   
             if autoit.win_active(" CentraLink - Login") :
@@ -240,13 +223,15 @@ def ImportCsvFile(CsvFilename=''):
     autoit.send("{Enter}")
     time.sleep(8)
     autoit.send("{esc}")
-    
+
+#退出Centralink  
 def LogoutCentralink():
     time.sleep(1)
     autoit.send("!S x")
     if autoit.win_wait_active(" Confirm exit",3):
           autoit.control_click(" Confirm exit", "Button1")
-
+          
+#读取配置文件
 def read_config(config_file_path, field, key): 
     cf = configparser.ConfigParser()
     try:
@@ -256,13 +241,13 @@ def read_config(config_file_path, field, key):
         sys.exit(1)
     return result
 
+#拷贝文件
 def CopyFile(SourceFile,DestinationFile):
       shutil.copyfile(SourceFile,DestinationFile)
 
 # End Define Function
 
 if __name__ == "__main__":
-      print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Start system')
       WriteLogFile('Start system')
       #1)Generate test.csv,mtd.csv,tstc.csv,sslf_imp.txt,Analyzer-Test-Codes.ini,Test-Information.ini 
       GenerateTestCsvFile()
@@ -273,29 +258,30 @@ if __name__ == "__main__":
       try:
             LoginCentralink()
             isLogin=True
-            print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Login Centralink successful')
             WriteLogFile('Login Centralink successful')
       except:
             isLogin=False
-            print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Login Centralink successful')
-            WriteLogFile('Login Centralink successful')
+            WriteLogFile('Login Centralink fail')
             
       #b)import .csv file to centralink database
-      sslfimpFile = os.getcwd()+r'\CSVFile\sslf_imp.txt'
-      try:
-            print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Start import .csv file to Centralink')
-            WriteLogFile('Start import .csv file to Centralink')
-            ImportCsvFile(sslfimpFile)
-            print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Import .csv file to Centralink successful')
-            WriteLogFile('Import .csv file to Centralink successful')
-      except :
-            print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Import .csv file to Centralink Fail')
+      if isLogin:
+            sslfimpFile = os.getcwd()+r'\CSVFile\sslf_imp.txt'
+            try:
+                  WriteLogFile('Start import .csv file to Centralink')
+                  ImportCsvFile(sslfimpFile)
+                  WriteLogFile('Import .csv file to Centralink successful')
+            except :
+                  WriteLogFile('Import .csv file to Centralink fail')
+      else:
             WriteLogFile('Import .csv file to Centralink fail')
+            
       #c)Logout Centralink
       if isLogin:
-            LogoutCentralink()
-            print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Logout Centralink')
-            WriteLogFile('Logout Centralink') 
+            try:
+                  LogoutCentralink()
+                  WriteLogFile('Logout Centralink') 
+            except:
+                  WriteLogFile('Logout Centralink fail') 
 
       #3)copy  Analyzer-Test-Codes.ini,Test-Information.ini to Aptio
       try:
@@ -306,9 +292,7 @@ if __name__ == "__main__":
             strSourceFileName=os.getcwd()+r'\CSVFile\Test-Information.ini'
             strDesFileName=strAptioConfigPath+r'\Test-Information.ini'
             CopyFile(strSourceFileName,strDesFileName)
-            print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Copy .ini file to Aptio successful')
             WriteLogFile('Copy .ini file to Aptio successful')
       except:
-            print('['+now.strftime('%Y-%m-%d %H:%M:%S')+']'+' '+'Copy .ini file to Aptio fail')
             WriteLogFile('Copy .ini file to Aptio fail')
                   
