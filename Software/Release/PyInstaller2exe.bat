510cd57282ef3249a983c175d9d10c237ca3987c
DaiHanpeng <AndySunshines@126.com>
2018-03-10 15:42:12 +0800

    add release V0.1

---
+++ Software/Release/PyInstaller2exe.bat
@@ -0,0 +1,8 @@
+E:\Python36-32\Scripts\pyinstaller ^
+-c ^
+--noupx ^
+--distpath=.  ^
+--hidden-import=queue ^
+--paths=E:\Python36-32\Lib\site-packages\PyQt5\Qt\plugins ^
+-p ..\UserInterface ^
+-F ..\UserInterface\Centralink_Start.py
