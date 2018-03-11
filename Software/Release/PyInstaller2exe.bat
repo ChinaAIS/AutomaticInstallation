53b4e21b320ea0bccace18de2f6bed0f755f9ca4
DaiHanpeng <andysunshines@126.com>
2018-03-10 20:59:37 -0800

    add comments in PyInstaller2exe.bat for compiling exe.

--- Software/Release/PyInstaller2exe.bat
+++ Software/Release/PyInstaller2exe.bat
@@ -1,3 +1,20 @@
+@echo off
+
+echo  *******************************************
+echo.
+echo  Automatic Installation Project Compiler
+echo  Author: Dai Han Peng / China Automation Team
+echo.
+echo  *******************************************
+rem pause
+
+rem -w is for Window
+rem -c is for Console
+rem --paths=E:\Python36-32\Lib\site-packages\PyQt5\Qt\plugins is the plugins for search Qt lib
+rem -p ..\UserInterface to add search paths for compiling
+
+echo start compile
+
 E:\Python36-32\Scripts\pyinstaller ^
 -w ^
 --noupx ^
@@ -6,3 +23,6 @@ E:\Python36-32\Scripts\pyinstaller ^
 --paths=E:\Python36-32\Lib\site-packages\PyQt5\Qt\plugins ^
 -p ..\UserInterface ^
 -F ..\UserInterface\Centralink_Start.py
+
+echo end compile
+pause
